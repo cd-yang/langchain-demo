a43b92ad35c5df86ed73975e23310c0855f8176e
CD Yang <cdyang1991@gmail.com>
2023-07-21 18:03:05 +0800

    chain call

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanMessage, ChatMessage, SystemMessage } from "langchain/schema";
 import dotenv from 'dotenv';
+import { LLMChain } from "langchain/chains";
+import {
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+} from "langchain/prompts";
+
 dotenv.config();
 
 const chat = new ChatOpenAI({
@@ -11,8 +18,22 @@ const chat = new ChatOpenAI({
     azureOpenAIApiVersion: process.env.AZURE_API_VERSION,
 });
 
-const result = await chat.predictMessages([
-    new HumanMessage("Translate this sentence from English to Japanese. I love programming.")
-]);
+const template = "You are a helpful assistant that translates {input_language} to {output_language}.";
+const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(template);
+const humanTemplate = "{text}";
+const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate(humanTemplate);
+
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([systemMessagePrompt, humanMessagePrompt]);
+
+const chain = new LLMChain({
+    llm: chat,
+    prompt: chatPrompt,
+});
+
+const result = await chain.call({
+    input_language: "English",
+    output_language: "Japanese",
+    text: "I love programming",
+});
 
 console.log(result);
